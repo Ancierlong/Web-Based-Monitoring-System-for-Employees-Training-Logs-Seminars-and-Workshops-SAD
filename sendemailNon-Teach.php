5ffb0eb2cb86857906c495b94f4437f60d1aaa45
Angelo Calong <114124125+Ancierlong@users.noreply.github.com>
2026-07-14 09:51:18 +0800

    Update sendemailNon-Teach.php

--- sendemailNon-Teach.php
+++ sendemailNon-Teach.php
@@ -163,13 +163,13 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp-relay.sendinblue.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'calongangelo1@gmail.com';
-    $mail->Password = 'xsmtpsib-d693d293770bdcaf5d0855b80ba4fcf00a3e71f1266824590bf0e2900f3a34f6-8m964YBJkWDUp5Ag';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->Port = 587;
 
     // Set email details
-    $mail->setFrom('calongangelo1@gmail.com');
-    $mail->addAddress('michael.valle@perpetualdalta.edu.ph');
+    $mail->setFrom('');
+    $mail->addAddress('');
     $mail->Subject = 'Request for Training Logs - Non-Teaching Classification';
 
     // Attach the query result as HTML
